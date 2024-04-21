6e1de8b8c009e69ac037973c74c60785e7f3109d
xiaoqi75643021 <hwx070618@163.com>
2024-04-21 12:45:50 +0800

    更新响应结构

--- internal/handler/response.go
+++ internal/handler/response.go
@@ -5,13 +5,13 @@ import "github.com/gin-gonic/gin"
 type Response struct {
 	Code    int    `json:"code"`
 	Message string `json:"message,omitempty"`
-	Content any    `json:"content,omitempty"`
+	Data    any    `json:"data,omitempty"`
 }
 
-func Respond(c *gin.Context, httpStatus, code int, msg string, content any) {
+func Respond(c *gin.Context, httpStatus, code int, msg string, data any) {
 	c.JSON(httpStatus, Response{
 		Code:    code,
 		Message: msg,
-		Content: content,
+		Data:    data,
 	})
 }
