1575cdf573228ea50c1970c1cf810c6ab4c31778
heweixin03 <heweixin03@meituan.com>
2024-04-26 18:10:10 +0800

    添加服务器优雅关闭

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,40 @@
 package main
 
 import (
+	"context"
+	"log"
+	"net/http"
 	"online-ordering-app/internal/router"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
 	r := router.SetupRouter()
-	r.Run()
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			panic(err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Panicln("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
+
+	log.Panicln("Server exiting")
 }
