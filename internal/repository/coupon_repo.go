4c5d26f0a3e58dcfca5ad0cd9946b1133d02cc92
heweixin03 <heweixin03@meituan.com>
2024-04-26 16:25:48 +0800

    合并聚合

--- internal/repository/coupon_repo.go
+++ internal/repository/coupon_repo.go
@@ -28,3 +28,27 @@ func ListCoupons(page, pageSize int) ([]*model.Coupon, error) {
 	err := database.DB.Offset((page - 1) * pageSize).Limit(pageSize).Find(&coupons).Error
 	return coupons, err
 }
+
+func CreateUserCoupon(userCoupon *model.UserCoupon) error {
+	return database.DB.Create(userCoupon).Error
+}
+
+func FindUserCouponByID(userCouponID uint) (*model.UserCoupon, error) {
+	userCoupon := new(model.UserCoupon)
+	err := database.DB.Where("id = ?", userCouponID).First(&userCoupon).Error
+	return userCoupon, err
+}
+
+func FindUserCouponsByUserID(userID uint) ([]*model.UserCoupon, error) {
+	var userCoupons []*model.UserCoupon
+	err := database.DB.Where("user_id = ?", userID).Find(&userCoupons).Error
+	return userCoupons, err
+}
+
+func UpdateUserCoupon(userCoupon *model.UserCoupon) error {
+	return database.DB.Save(userCoupon).Error
+}
+
+func DeleteUserCoupon(id uint) error {
+	return database.DB.Delete(&model.UserCoupon{}, id).Error
+}
